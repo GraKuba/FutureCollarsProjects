34c777cfbd3d1861b6501ab7091c4f636ce77839
Jakub Grabarczyk <77164148+GraKuba@users.noreply.github.com>
2023-03-17 17:46:06 +0100

    File path for download has been created

--- reader.py
+++ reader.py
@@ -1,5 +1,22 @@
 # IMPORTS 
 import sys
 import pickle
-import sys
+import csv
 import json
+
+# FILE PATH FOR THE DOWNLOAD
+file = '/Users/kubagrabarczyk/Desktop/python recap/input.csv'
+tempData = []
+with open(file, 'r') as f:
+    reader = csv.reader(f)
+    for line in reader:
+        print(' '.join(line))
+    for line in reader:
+        tempData.append(line)
+
+
+# output = "/Users/kubagrabarczyk/Desktop/python recap/output.csv"
+# with open(output, 'w') as f:
+#     writer = csv.writer(f)
+#     for line in tempData:
+#         writer.writerow(line)
