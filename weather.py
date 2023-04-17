1afbe92f02c9172acda50b3ee52d2c813ab06a98
Jakub Grabarczyk <77164148+GraKuba@users.noreply.github.com>
2023-04-17 17:48:17 +0200

    class WeatherForecast fixed, program now returning Rain, No rain, or Don't know

--- weather.py
+++ weather.py
@@ -10,10 +10,30 @@ class WeatherForecast:
 		self.url = url
 		self.list = []
 
-	def checkInData(self):
-		for i in self.list:
-			self.list.append(i)
-		return self.list
+	def analyseData(self, data):
+		dateList = []
+		for idx in data['list']:
+			date = idx['dt_txt'].split(' ', 1)[0]
+			weatherTpl = {date: idx['weather'][0]['main']}
+			dateList.append(weatherTpl)
+	
+		knownDates = []
+		for idx in dateList:
+			for key in idx.keys():
+				knownDates.append(key)
+
+		if sys.argv[2] not in knownDates:
+			return print("Date not known")
+		else:
+			rainyDays = []
+			for idx in dateList:
+				for key in idx:
+					if idx[key] == 'Rain':
+						rainyDays.append(key)
+			if sys.argv[2] in rainyDays:
+				return print('Rain')
+			else:
+				return print('No rain')
 
 	def addToData(self):
 		response = requests.get(self.url).json()	
@@ -23,25 +43,4 @@ wf = WeatherForecast(url)
 
 data = wf.addToData()
 
-# pprint.pprint(data)
-
-dateList = []
-for idx in data['list']:
-	date = idx['dt_txt'].split(' ', 1)[0]
-	weatherTpl = {date: idx['weather'][0]['main']}
-	dateList.append(weatherTpl)
-
-for idx in dateList:
-	for i,j in idx.items():
-		
-
-
-
-# weatherList = []
-# for idx in dateList:
-# 	if idx[1] == 'Rain': 
-
-# print(weatherList)
-
-# if test == sys.argv[2]:
-# 	print('yes')
+wf.analyseData(data)
